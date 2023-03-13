9452963a9465dad208fb636f01fbfd0d456e47d4
AriestaTegar-JR <ariestategarjr87@gmail.com>
2023-03-13 10:17:20 +0700

    make migrations and seeds Products

---
+++ app/Database/Migrations/2023-03-13-030118_CreateProductsTable.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_produk' => [
+                'type' => 'VARCHAR',
+                'constraint' => 11,
+                'unique' => true,
+            ],
+			'kode_barcode' => [
+				'type' => 'VARCHAR',
+				'constraint' => 50,
+                'unique' => true,
+			],
+			'nama_produk' => [
+				'type' => 'VARCHAR',
+				'constraint' => 100,
+			],
+			'id_satuan_produk' => [
+				'type' => 'VARCHAR',
+				'constraint' => 11,
+                'unique' => true,
+			],
+			'id_kategori_produk' => [
+				'type' => 'VARCHAR',
+				'constraint' => 11,
+                'unique' => true,
+			],
+			'stok' => [
+				'type' => 'DOUBLE',
+				'constraint' => '11,2',
+				'default' => 0.00
+			],
+			'harga_beli' => [
+				'type' => 'DOUBLE',
+				'constraint' => '11,2',
+				'default' => 0.00
+			],
+			'harga_jual' => [
+				'type' => 'DOUBLE',
+				'constraint' => '11,2',
+				'default' => 0.00
+            ],
+            'created_at' => [
+                'type' => 'DATETIME DEFAULT CURRENT_TIMESTAMP'
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME ON UPDATE CURRENT_TIMESTAMP'
+            ]
+		]);
+
+		$this->forge->addKey('id_produk', true);
+		$this->forge->addForeignKey('id_satuan_produk', 'tb_satuan', 'id_satuan', 'cascade');
+		$this->forge->addForeignKey('id_kategori_produk', 'tb_kategori', 'id_kategori', 'cascade');
+		$this->forge->createTable('tb_produk');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_produk');
+    }
+}
