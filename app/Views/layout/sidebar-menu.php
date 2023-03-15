9c4ff182150742d1c4c259ed6e051263e3ac2a50
AriestaTegar-JR <ariestategarjr87@gmail.com>
2023-03-15 10:00:45 +0700

    separate sidebar and sidebar-menu

--- app/Views/layout/sidebar-menu.php
+++ app/Views/layout/sidebar-menu.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-    <a href="<?= site_url('layout/') ?>" class="nav-link">
+    <a href="<?= site_url('/') ?>" class="nav-link">
         <i class="nav-icon far fa-tachometer-alt"></i>
         <p>
             Home
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -82,35 +82,7 @@
 
         <?= $this->include('layout/navbar') ?>
 
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-
-            <a href="<?= base_url('assets') ?>/index3.html" class="brand-link">
-                <img src="<?= base_url('assets') ?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">POS</span>
-            </a>
-
-            <div class="sidebar">
-
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="<?= base_url('assets') ?>/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
-                    </div>
-                </div>
-
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
-                        <?= $this->include('layout/menu') ?>
-
-                    </ul>
-                </nav>
-
-            </div>
-
-        </aside>
+        <?= $this->include('layout/sidebar') ?>
 
         <div class="content-wrapper">
 
