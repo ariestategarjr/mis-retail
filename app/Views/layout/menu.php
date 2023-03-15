12a730b1c3637e1f0da645cb31470ee0f2e34892
AriestaTegar-JR <ariestategarjr87@gmail.com>
2023-03-15 08:44:27 +0700

    arrange layout template and partials menu and navbar

---
+++ app/Views/layout/menu.php
@@ -0,0 +1,8 @@
+<li class="nav-item">
+    <a href="<?= site_url('layout/home') ?>" class="nav-link">
+        <i class="nav-icon far fa-tachometer-alt"></i>
+        <p>
+            Home
+        </p>
+    </a>
+</li>
---
+++ app/Views/pages/home.php
@@ -0,0 +1,5 @@
+<?= $this->extend('layout/template') ?>
+
+<?= $this->section('content') ?>
+<h1>Ini adalah halaman Home</h1>
+<?= $this->endSection() ?>
