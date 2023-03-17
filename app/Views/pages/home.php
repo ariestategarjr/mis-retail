cd43da13e8db5b38197f75688f5295ea2462662d
AriestaTegar-JR <ariestategarjr87@gmail.com>
2023-03-17 08:13:08 +0700

    delete entire lasst project

--- app/Views/pages/home.php
+++
@@ -1,9 +0,0 @@
-<?= $this->extend('layout/menu') ?>
-
-<?= $this->section('content') ?>
-<div class="alert alert-info alert-dismissible">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <h5><i class="icon fas fa-info"></i> Selamat Datang!</h5>
-    Ini adalah Aplikasi Point Of Sales.
-</div>
-<?= $this->endSection() ?>
