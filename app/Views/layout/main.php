77ee880adbba1d8c88bf09d01a6e1185282971f3
AriestaTegar-JR <ariestategarjr87@gmail.com>
2023-04-28 23:27:44 +0700

    change alexander-pierce name

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -66,7 +66,7 @@
                         <img src="<?= base_url('assets') ?>/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">User</a>
                     </div>
                 </div>
 
