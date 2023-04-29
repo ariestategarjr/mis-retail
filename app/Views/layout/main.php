7921cfd3959c814deae1915a41f4599552cff553
AriestaTegar-JR <ariestategarjr87@gmail.com>
2023-04-29 08:40:07 +0700

    change POS name to Cashier

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -55,7 +55,7 @@
             <!-- Brand Logo -->
             <a href="<?= base_url('assets') ?>/index3.html" class="brand-link">
                 <img src="<?= base_url('assets') ?>/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">POS</span>
+                <span class="brand-text font-weight-light">Cashier</span>
             </a>
 
             <!-- Sidebar -->
