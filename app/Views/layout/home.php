eb4f0017e1ab67427d9fc3b5d0c221ed057e8a94
AriestaTegar-JR <ariestategarjr87@gmail.com>
2023-04-30 10:02:16 +0700

    change home welcome message

--- app/Views/layout/home.php
+++ app/Views/layout/home.php
@@ -9,6 +9,6 @@
 <div class="alert alert-info alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <h5><i class="icon fas fa-info"></i> Selamat Datang!</h5>
-    Ini adalah aplikasi Point of Sales.
+    Ini adalah aplikasi Kasir.
 </div>
 <?= $this->endSection(); ?>
